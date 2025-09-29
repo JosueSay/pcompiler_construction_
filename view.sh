5a12395345e551e398b71ed304f7c2f6cbf4774b
JosueSay <josuesay770@gmail.com>
2025-09-28 19:07:44 -0600

    feat: incluyendo poblacion test sh

---
+++ view.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Ruta de los .cps
+RUTA="./src/test/tac"
+
+# Mostrar contenidos de los archivos
+for f in "$RUTA"/*.cps; do
+  echo "===== $f ====="
+  cat "$f"
+  echo
+done
+
+# Recorre todos los archivos .cps y los pasa a run.sh
+for f in "$RUTA"/*.cps; do
+  echo ">> Ejecutando $f"
+  CPS_VERBOSE=0 ./scripts/run.sh "$f"
+  echo ""
+done
