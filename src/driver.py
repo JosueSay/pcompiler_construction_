68c90edbe074bbf1140dcc4ff8e2378536b380ea
JosueSay <josuesay770@gmail.com>
2025-11-14 12:18:39 -0600

    feat: implementación tac

--- src/driver.py
+++ src/driver.py
@@ -6,13 +6,14 @@ from antlr_gen.CompiscriptParser import CompiscriptParser
 
 from semantic.visitor import VisitorCPS
 
-# Logger & reports (nueva API)
+# logger
 from logs.logger import startRun, log, currentOutDir
 from logs.reports import writeSymbolsLog, writeAstText, writeAstHtml
 
-# IR / TAC
+# IR / TAC / CG
 from ir.emitter import Emitter
 from ir.tac_generator import TacGenerator
+from cg.mips_generator import generateMipsFromTac
 
 
 def outputStem(src_path: str) -> str:
@@ -63,7 +64,7 @@ def main(argv: list[str]) -> None:
     visitor_sem.visit(tree)
     t_sem1: float = time.perf_counter()
 
-    # ---------- Fase TAC (segunda pasada, solo emisión) ----------
+    # ---------- Fase TAC ----------
     t_tac0: float = time.perf_counter()
     emitter_tac: Emitter = Emitter(program_name=stem)
     tac_gen: TacGenerator = TacGenerator(visitor_sem, emitter_tac)
@@ -79,8 +80,15 @@ def main(argv: list[str]) -> None:
     tac_txt: str = tac_gen.emitter.writeTacText(out_dir, stem, simple_names=True)
     tac_html: str = tac_gen.emitter.writeTacHtml(out_dir, stem, simple_names=True)
 
+    # ---------- Fase MIPS ----------
+    t_mips0: float = time.perf_counter()
+    mips_path = generateMipsFromTac(tac_txt, out_dir, stem)
+    t_mips1: float = time.perf_counter()
+
+
     log(f"[TAC] escrito: {tac_txt}", channel="semantic", force=True)
     log(f"[TAC] escrito: {tac_html}", channel="semantic", force=True)
+
 
     # ---------- Errores ----------
     if getattr(visitor_sem, "errors", None):
@@ -93,23 +101,26 @@ def main(argv: list[str]) -> None:
     dt_syn = t_syn1 - t_syn0
     dt_sem = t_sem1 - t_sem0
     dt_tac = t_tac1 - t_tac0
-    dt_tot = dt_lex + dt_syn + dt_sem + dt_tac
+    dt_mips = t_mips1 - t_mips0
+    dt_tot = dt_lex + dt_syn + dt_sem + dt_tac + dt_mips
+
 
     print(f"{GREEN}{BOLD}Tiempos:{RESET}")
     print(f"\t{CYAN}Léxica:     {RESET}{dt_lex:.3f}s")
     print(f"\t{CYAN}Sintáctica: {RESET}{dt_syn:.3f}s")
     print(f"\t{CYAN}Semántica:  {RESET}{dt_sem:.3f}s")
     print(f"\t{CYAN}TAC:        {RESET}{dt_tac:.3f}s")
+    print(f"\t{CYAN}MIPS:       {RESET}{dt_mips:.3f}s")
     print(f"{GREEN}{BOLD}Tiempo total:{RESET} {dt_tot:.3f}s")
 
     print(f"{CYAN}{BOLD}OUT DIR:{RESET} {out_dir}")
     print(f"{CYAN}{'='*120}{RESET}")
     
-        # ---------- Imprimir TAC al finalizar ----------
-    print(f"{MAGENTA}{BOLD}=== TAC generado ==={RESET}")
-    with open(tac_txt, "r", encoding="utf-8") as f:
-        print(f.read())
-    print(f"{MAGENTA}{BOLD}===================={RESET}")
+    # ---------- Imprimir TAC al finalizar ----------
+    # print(f"{MAGENTA}{BOLD}=== TAC generado ==={RESET}")
+    # with open(tac_txt, "r", encoding="utf-8") as f:
+    #     print(f.read())
+    # print(f"{MAGENTA}{BOLD}===================={RESET}")
 
 
 if __name__ == "__main__":
